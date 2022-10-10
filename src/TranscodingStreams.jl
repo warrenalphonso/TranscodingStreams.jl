a79480546c5582afd93497a338f0d2a23c605e2e
Warren Alphonso <warrenalphonso02@gmail.com>
2022-10-10 22:12:48 +0200

    From scratch

--- src/TranscodingStreams.jl
+++ src/TranscodingStreams.jl
@@ -1,19 +1,3 @@
 module TranscodingStreams
 
-export
-    TranscodingStream,
-    Noop,
-    NoopStream
-
-const ByteData = Union{Vector{UInt8},Base.CodeUnits{UInt8}}
-
-include("memory.jl")
-include("buffer.jl")
-include("codec.jl")
-include("state.jl")
-include("stream.jl")
-include("io.jl")
-include("noop.jl")
-include("transcode.jl")
-
 end # module
